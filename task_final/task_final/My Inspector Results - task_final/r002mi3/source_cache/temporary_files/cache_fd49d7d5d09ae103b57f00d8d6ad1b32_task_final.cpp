20a2b82ac1b8779b5df6124ce876a97e18b2c295
Nastasiya <shvecovaanastasiyacsf@gmail.com>
2019-12-27 00:40:31 +0300

    итоговое задание

---
+++ task_final/task_final/My Inspector Results - task_final/r002mi3/source_cache/temporary_files/cache_fd49d7d5d09ae103b57f00d8d6ad1b32_task_final.cpp
@@ -0,0 +1,43 @@
+﻿// task_final.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include "fragmentation.h"
+#include <locale.h>
+#include <chrono>
+#include <iostream>
+
+using namespace std::chrono;
+
+/// параметры начальной прямоугольной области
+double g_l1_max = 12.0;
+double g_l2_max = g_l1_max;
+double g_l1_min = 8.0;
+double g_l2_min = g_l1_min;
+double g_l0 = 5.0;
+
+/// точность аппроксимации рабочего пространства
+double g_precision = 0.25;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	high_level_analysis main_object;
+	
+	const char* out_files[3] = { "solution.txt", "boundary.txt", "not_solution.txt" };
+
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
+	main_object.GetSolution();
+
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	duration<double> duration = (t2 - t1);
+
+	printf("Duration serial is: %f seconds \n\n", duration.count());
+
+	//WriteResults(out_files);
+
+	return 0;
+}
---
+++ task_final/task_final/task_final.cpp
@@ -0,0 +1,49 @@
+﻿// task_final.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include "fragmentation.h"
+#include <locale.h>
+#include <chrono>
+#include <iostream>
+#include <cilk/cilk_api.h>
+#include <cilk/cilk.h>
+
+using namespace std::chrono;
+
+/// параметры начальной прямоугольной области
+double g_l1_max = 12.0;
+double g_l2_max = g_l1_max;
+double g_l1_min = 8.0;
+double g_l2_min = g_l1_min;
+double g_l0 = 5.0;
+
+/// точность аппроксимации рабочего пространства
+double g_precision = 0.05;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	__cilkrts_end_cilk();
+	__cilkrts_set_param("nworkers", "4");
+
+	high_level_analysis main_object;
+	
+	const char* out_files[3] = { "solution.txt", "boundary.txt", "not_solution.txt" };
+
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
+	main_object.GetSolution();
+
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	duration<double> duration = (t2 - t1);
+
+	//printf("Duration serial is: %f seconds \n\n", duration.count());
+	//std::cout << "Number of workers " << __cilkrts_get_nworkers() << std::endl;
+
+	WriteResults(out_files);
+
+	return 0;
+}
